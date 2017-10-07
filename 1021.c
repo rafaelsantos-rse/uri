a0cac65dc193e38ef3fd41ab3f2537e8feff87c9
rafaelsantos-rse <rafaelsantos.rse@gmail.com>
2017-10-07 18:05:35 -0300

    hope it works

--- 1021.c
+++ 1021.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 int main(void){
-	void decompor(float a, float b);
-	int Lreais[6] = {100, 50, 20, 10, 5, 2 }, i;
-	float Lcentavos[6] = {1, 0.5, 0.25, 0.10, 0.05, 0.01}, dinheiro;
+	void decomporR(float a, int *B);
+	void decomporC(float a, float *B);
+	void *p;
+	int Reais[6] = {100, 50, 20, 10, 5, 2 }, i;
+	float Centavos[6] = {1.0, 0.5, 0.25, 0.10, 0.05, 0.01}, dinheiro;
 	scanf("%f",&dinheiro);
-	for (i = 0; i < 11; i++){
-		if(i < 6){
-			decompor(dinheiro, Lreais[i]);
-		}
-		else{
-			i -= 6;
-			decompor(dinheiro, Lcentavos[i]);
-		}
+	p = &Reais[0];
+	for (i = 0; i < 6; i++){
+		decomporR(dinheiro, p);
+		++p;
+	}
+	p = &Centavos[0];
+	for (i = 0; i < 6; i++){
+		decomporC(dinheiro, p);
+		p++;
 	}
 	return 0;
 }
-void decompor(float a, float b){
+void decomporR(float a, int *B){
+	int b = *B;
 	if(b <= 100 && b >= 2){
 		int c = 0;
-		if(a >= b){
-			b = (int) b;
-			int c = (int) ( a - (a% b) )
-			a -= c;
-			c /= b;
+		while (a >= b){
+			a -= b;
+			c++;
 		}
-		printf("%d nota(s) de R$ %d.00\n",c, b);
+		printf("%d nota(s) de R$ %d.00\n", c, b);
 	}
-	else if( b <= 1 && b >= 0.01){
-	 	int c = 0 
+}
+void decomporC(float a, float *B){
+ 	float b  = *B;
+	if(b <= 1 && b >= 0.01){
+	 	int c = 0;
 	 	while (a >= b){
 	 		a -= b;
 	 		c++;
