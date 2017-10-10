65abb81699a0dc40ce3a0c1b21970132119c77e9
Rafael Santos <rafaelsantos.rse@gmail.com>
2017-10-10 09:53:31 -0300

    ...

--- 1045.c
+++ 1045.c
@@ -6,14 +6,19 @@ software done by: Rafael Santos
 #include <stdio.h>
 
 int main(void){
-	void triangleTest(double a,double b,double c);
-	double a, b, c;
-	scanf("%lF %lF %lF", &a, &b, &c);
-	triangleTest(a, b, c);
+	void triangleTest(double *v);
+	void sort(double *v, int length);
+	int length = 3;
+	double v[length];
+	scanf("%lF %lF %lF", &v[0], &v[1], &v[2]);
+	sort(v, length);
+	/*printf("%.1lf %.1lf %.1lf\n", v[0], v[1], v[2]);*/
+	triangleTest(v);
 	return 0;
 }
 
-void triangleTest(double a, double b,double c){
+void triangleTest(double *v){
+	double c = v[0], b = v[1], a = v[2];
 	if( a >= (b + c)){
 		printf("NAO FORMA TRIANGULO\n");
 	} else{
@@ -33,6 +38,20 @@ void triangleTest(double a, double b,double c){
 		else if(( a == b && a != c ) || (a != b && a == c) || (b == c && b != a)){
 			printf("TRIANGULO ISOSCELES\n");
 		}
+	}
+
+}
+void sort(double *v, int length){
+	int j, i;
+	double k;
+	for (j = 1; j < length; j++){
+		i = j - 1;
+		k = v[j];
+		while (i >= 0 && v[i] > k){
+			v[i+1] = v[i];
+			i--;
+		}
+		v[i+1]= k;
 	}
 
 }
